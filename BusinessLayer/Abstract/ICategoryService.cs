5d7464b588d2e009566b5a434b6694fdc3ec0fad
Başak Dağdelen <139124230+Basakdgdln@users.noreply.github.com>
2023-07-24 21:44:03 +0300

    Add project files.

---
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+   public interface ICategoryService
+    {
+        List<Category> CategoryAllist();
+        void CategoryAdd(Category c);
+        void CategoryDelete(Category c);
+        void CategoryUpdate(Category c);
+        Category GetById(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository efCategoryRepository;
+        public CategoryManager()
+        {
+            efCategoryRepository = new EfCategoryRepository();
+        }
+
+        public void CategoryAdd(Category c)
+        {
+            efCategoryRepository.Insert(c);
+        }
+
+        public List<Category> CategoryAllist()
+        {
+          return  efCategoryRepository.GetListAll();
+        }
+
+        public void CategoryDelete(Category c)
+        {
+            efCategoryRepository.Delete(c);
+        }
+
+        public void CategoryUpdate(Category c)
+        {
+            efCategoryRepository.Update(c);
+        }
+
+        public Category GetById(int id)
+        {
+            return efCategoryRepository.GetById(id);
+        }
+    }
+}
