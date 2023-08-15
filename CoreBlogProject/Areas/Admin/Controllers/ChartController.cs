f03e3f225adf067800293d9a0fa9fe2b808f6ea2
Başak Dağdelen <139124230+Basakdgdln@users.noreply.github.com>
2023-08-15 21:24:14 +0300

    add

---
+++ CoreBlogProject/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,44 @@
+﻿using CoreBlogProject.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlogProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+            list.Add(new CategoryClass
+            {
+                categorycount = 10,
+                categoryname = "Teknoloji"
+            });
+            list.Add(new CategoryClass
+            {
+                categorycount = 14,
+                categoryname = "Yazılım"
+            });
+            list.Add(new CategoryClass
+            {
+                categorycount = 5,
+                categoryname = "Spor"
+            });
+            list.Add(new CategoryClass
+            {
+                categorycount = 2,
+                categoryname = "Sinema"
+            });
+            return Json(new { jsonlist = list });
+        }
+    }
+}
